ec40441b1d4339d0cf246d31bc6c359c900a4176
nacho <xxnachetoxx@gmail.com>
2024-06-13 18:21:49 +0200

    Arcivos hasta el video 16

---
+++ App1-practica/main.js
@@ -0,0 +1,14 @@
+const {app,BrowserWindow}=require('electron')
+const path= require('path')
+
+function crearVentanaPrincipar(){
+    let ventanaPrincipal=new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname,'preload.js')
+        }
+    });
+    ventanaPrincipal.loadFile('index.html');
+}
+app.whenReady().then(crearVentanaPrincipar)
