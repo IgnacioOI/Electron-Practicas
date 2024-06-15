79bff2d82faf9ab7c1d70da47146e51c4d5d78fe
nacho <xxnachetoxx@gmail.com>
2024-06-15 19:29:00 +0200

    Practica del video 31 al 40 con arreglos

--- App2-Marcadores/main.js
+++ App2-Marcadores/main.js
@@ -1,13 +1,29 @@
-const {app, BrowserWindow} = require('electron');
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
 function crearVentanaPrincipal() {
     let ventanaPrincipal = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
-            nodeIntegration: true
+            nodeIntegration: true,
+            contextIsolation: false 
         }
     });
 
     ventanaPrincipal.loadFile('index.html');
 }
+
 app.whenReady().then(crearVentanaPrincipal);
+
+app.on('window-all-closed', function() {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', function() {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        crearVentanaPrincipal();
+    }
+});
--- App2-Marcadores/renderer.js
+++ App2-Marcadores/renderer.js
@@ -1,61 +1,101 @@
-class Marcadores{
-    constructor(){
-        this.mensajeError=document.querySelector('.mensaje-error')
-        this.marcadorUrl=document.querySelector('.creacion-marcador-url');
-        this.marcadorBoton=document.querySelector('creacion-marcador-boton');
-        this.marcadorBoton=document.querySelector('creacion-marcador-boton');
-        this.marcadores=document.querySelector('.marcadores');
-        this.eliminarMarcadores=document.querySelector('.borrar-marcadores');
-        this.parser= new DOMParser();
+const { shell } = require('electron');
+
+class Marcadores {
+    constructor() {
+        this.mensajeError = document.querySelector('.mensaje-error');
+        this.formularioCreacionMarcadores = document.querySelector('.creacion-marcador-formulario');
+        this.marcadorUrl = document.querySelector('.creacion-marcador-url');
+        this.marcadorBoton = document.querySelector('.creacion-marcador-boton');
+        this.marcadores = document.querySelector('.marcadores');
+        this.eliminarMarcadores = document.querySelector('.remover-marcadores');
+
+        this.parser = new DOMParser();
+
         this.agregarEventListeners();
-
     }
-            
-    agregarEventListeners(){
-        this.marcadorUrl.addEventListener('keyup',()=>{
-            this.marcadorBoton.disabled=!this.marcadorUrl.validity.valid;
+
+    agregarEventListeners() {
+        this.marcadorUrl.addEventListener('keyup', () => {
+            this.marcadorBoton.disabled = !this.marcadorUrl.validity.valid;
         });
-        this.marcadorBoton.addEventListener('submit',this.crearMarcador.bind(this));
 
+        this.formularioCreacionMarcadores.addEventListener('submit', this.crearMarcador.bind(this));
+
+        this.eliminarMarcadores.addEventListener('click', this.eliminarMarcadoresCreados.bind(this));
+
+        this.marcadores.addEventListener('click', this.abrirEnlaceMarcador.bind(this));
     }
-    crearMarcador(evento){
+
+    crearMarcador(evento) {
         evento.preventDefault();
-        const url=this.marcadorUrl.nodeValue;
+
+        const url = this.marcadorUrl.value;
+
         fetch(url)
-        .then(respuesta=>respuesta.text())
-        .then(this.extraerContenido)
-        .then(this.encontrarTituloPagina)
-        .then(titulo=>this.almacenarMaracardor(url,titulo))
-        .then(this.limpiarFormulario)
-        .then(visualizarMarcadores)
-        .catch(eror => this.reportarError(error,))
+            .then(respuesta => respuesta.text())
+            .then(this.extraerContenido.bind(this))
+            .then(this.encontrarTituloPagina)
+            .then(titulo => this.almacenarMarcador(url, titulo))
+            .then(this.limpiarFormulario.bind(this))
+            .then(this.visualizarMarcadores.bind(this))
+            .catch(error => this.reportarError(error, url));
     }
-    extraerContenido(contenido){
-        return this.parser.parseFromString(contenido, 'text/html')
+
+    extraerContenido(contenido) {
+        console.log(contenido);
+        return this.parser.parseFromString(contenido, 'text/html');
     }
-    encontrarTituloPagina(html){
+
+    encontrarTituloPagina(html) {
         return html.querySelector('title').innerText;
     }
-    almacenarMaracardor(url, titulo){
-        localStorage.setItem(url,JSON.stringify({titulo:titulo, html:html}))
+
+    almacenarMarcador(url, titulo) {
+        localStorage.setItem(url, JSON.stringify({ titulo: titulo, url: url }));
     }
-    limpiarFormulario(){
-        this.marcadorUrl.value=null;
+
+    limpiarFormulario() {
+        this.marcadorUrl.value = null;
     }
-    obtenerEnlaces(){
+
+    obtenerMarcadores() {
         return Object.keys(localStorage).map(k => JSON.parse(localStorage.getItem(k)));
     }
-    generarHtmlMarcador(marcador){
-        return `<div class="enlace"><h3>${marcador.titulo}</h3>
-        <p><a href="${marcador.url}">${marcador.url}</a></p></div>`;
-    }
-    visualizarMarcadores(){
-        let enlaces=this.obtenerEnlaces();
 
-        let html= marcadores.map(this.generarHtmlMarcador().join(''));
-        this.mensajeError.innerHTML=html
+    generarHtmlMarcador(marcador) {
+        return `<li class="list-group-item"><h4>${marcador.titulo}</h4><a href="${marcador.url}">${marcador.url}</a></li>`;
     }
-    reportarError(error,url){
-        
+
+    visualizarMarcadores() {
+        let marcadores = this.obtenerMarcadores();
+
+        let html = marcadores.map(this.generarHtmlMarcador).join('');
+
+        this.marcadores.innerHTML = `<ul class="list-group">${html}</ul>`;
+    }
+
+    reportarError(error, url) {
+        this.mensajeError.classList.remove('invisible');
+        this.mensajeError.innerHTML = `Ocurrió un error al intentar acceder a ${url}: ${error}`;
+
+        setTimeout(() => {
+            this.mensajeError.innerText = null;
+            this.mensajeError.classList.add('invisible');
+        }, 5000);
+    }
+
+    eliminarMarcadoresCreados() {
+        localStorage.clear();
+        this.marcadores.innerHTML = '';
+    }
+
+    abrirEnlaceMarcador(evento) {
+        if (evento.target.href) {
+            evento.preventDefault();
+            shell.openExternal(evento.target.href);
+        }
     }
 }
+
+let marcadores = new Marcadores();
+marcadores.visualizarMarcadores();
