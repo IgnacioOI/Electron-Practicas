505880eb62978b5915dd173bab15916615f68816
nacho <xxnachetoxx@gmail.com>
2024-06-19 19:33:03 +0200

    Video de 54 al 64 arreglos

--- App3-Gestor-Personas/main.js
+++ App3-Gestor-Personas/main.js
@@ -1,29 +1,66 @@
 const {app, BrowserWindow, Menu} = require('electron');
+
 let ventanaPrincipal;
+
 let menuAplicacionPlantilla = [
     {
-        label: 'Archivo',
+        label: 'Aplicación',
         submenu: [
             {
-                label: 'Agregar producto',
-                click() {
-                    crearVentanaAgregarProducto()
+                label: 'Acerca de',
+                click: () => {
+                    abrirVentanaAcercaDe();
                 }
             }
         ]
     }
 ];
-function crearVentanaPrincipal() {
-    let ventanaPrincipal = new BrowserWindow({
+
+function crearVentanaPrincipal(){
+    ventanaPrincipal = new BrowserWindow({
         width: 800,
-        height: 600,
+        height: 700,
         webPreferences: {
-            nodeIntegration: true,
+            nodeIntegration: true
         }
     });
 
     ventanaPrincipal.loadFile('index.html');
-    let menu= Menu.buildFromTemplate(menuAplicacionPlantilla);
-    ventanaPrincipal.setMenu(menu)
+
+    let menu = Menu.buildFromTemplate(menuAplicacionPlantilla);
+    ventanaPrincipal.setMenu(menu);
+
+    ventanaPrincipal.on('closed', () => {
+        ventanaPrincipal = null;
+    });
 }
+
+function abrirVentanaAcercaDe() {
+    let ventanaAcercaDe = new BrowserWindow({
+        parent: ventanaPrincipal,
+        modal: true,
+        show: false,
+        width: 400,
+        height: 250
+    });
+
+    ventanaAcercaDe.loadFile('acerca-de.html');
+    ventanaAcercaDe.setMenu(null);
+    ventanaAcercaDe.once('ready-to-show', () => {
+        ventanaAcercaDe.show();
+    });
+}
+
 app.whenReady().then(crearVentanaPrincipal);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin'){
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (ventanaPrincipal === null){
+        crearVentanaPrincipal();
+    }
+});
